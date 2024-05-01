513c12ab083fcd782a857ceb0d351e221b893cee
MarvsTech <115031416+Crispkreme@users.noreply.github.com>
2024-04-30 16:22:25 -0800

    Update route.ts

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,6 +5,10 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300;
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export async function GET() {
     
     try {
@@ -36,7 +40,7 @@ export async function GET() {
                 }
 
                 const updatedProduct = await Product.findOneAndUpdate(
-                    { url: scrapedProduct.url },
+                    { url: product.url },
                     product,
                 );
 
@@ -63,7 +67,7 @@ export async function GET() {
             message: 'OK',
             data: updatedProducts
         });
-        
+
     } catch (error) {
         throw new Error(`Error in GET: ${error}`);
     }
